2ebda4ca4fc68a1ce427a26359eeccd2c12aff39
Naveen Venkataiah <naveenblaze44@gmail.com>
2024-12-05 16:40:18 -0500

    multiple render bugs fixed

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-// Document api code to be added in here
-const todoInput = document.getElementById("todo-input");
-const addTaskButton = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
-addTaskButton.addEventListener("click", () => {
-  saveTasks();
-});
+  let localtasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-todoList.addEventListener("click", (e) => {
-  console.log(e.target);
-  if (e.target.tagName === "BUTTON") {
-    // e.target.parentElement.remove();
-    e.target.parentElement.classList.toggle("completed");
+  localtasks.forEach((task) => {
+    renderTasks(task);
+  });
+
+  addTaskButton.addEventListener("click", () => {
+    saveTasks();
+  });
+
+  todoList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      // e.target.parentElement.remove();
+      e.target.parentElement.classList.toggle("completed");
+    }
+  });
+  function saveTasks() {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return;
+    let taskDefinition = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    localtasks.push(taskDefinition);
+    renderTasks(taskDefinition);
+    localStorage.setItem("tasks", JSON.stringify(localtasks));
+    todoInput.value = "";
+  }
+
+  // TODO Need to implement local storage
+  function renderTasks(taskItem) {
+    const li = document.createElement("li");
+    li.textContent = taskItem.text;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "delete";
+    li.appendChild(deleteButton);
+    todoList.appendChild(li);
   }
 });
-function saveTasks() {
-  const taskText = todoInput.value.trim();
-  if (taskText === "") return;
-  const li = document.createElement("li");
-  li.textContent = taskText;
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "delete";
-  li.appendChild(deleteButton);
-  todoList.appendChild(li);
-  todoInput.value = "";
-}
-
-// TODO Need to implement local storage
